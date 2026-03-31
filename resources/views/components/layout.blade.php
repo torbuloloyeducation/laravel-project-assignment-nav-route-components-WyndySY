c9d57327927f41a92ddfbf890c1f4052ee4acd2a
WyndySY <w.yecyec.146939.tc@umindanao.edu.ph>
2026-03-31 16:09:58 +0800

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,17 +11,6 @@
         background-color: grey;
     }
 </style>
-<body>
-{{ $slot }}
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Simple Navigation</title>
 </head>
 <body>
     <x-navbar />
